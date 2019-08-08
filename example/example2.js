24d93b907f1f80ca88ab6830bf46355a9826b592
ytb2mp3 <ytb2mp3mail@protonmail.com>
2019-08-08 14:49:13 +0200

    Fix example

--- example/example2.js
+++ example/example2.js
@@ -1,16 +1,17 @@
 var YoutubeMp3Downloader = require("../index");
+var os = require("os");
 
 //Configure YoutubeMp3Downloader with your settings
 var YD = new YoutubeMp3Downloader({
-    "ffmpegPath": "/path/to/ffmpeg",        // Where is the FFmpeg binary located?
-    "outputPath": "/path/to/mp3/folder",    // Where should the downloaded and encoded files be stored?
+    "ffmpegPath": "/usr/local/bin/ffmpeg",  // Where is the FFmpeg binary located?
+    "outputPath": os.tmpdir(),              // Where should the downloaded and encoded files be stored?
     "youtubeVideoQuality": "highest",       // What video quality should be used?
     "queueParallelism": 2,                  // How many parallel downloads/encodes should be started?
     "progressTimeout": 2000                 // How long should be the interval of the progress reports
 });
 
 //Download video and save as MP3 file
-YD.download("Vhd6Kc4TZls", "Cold Funk - Funkorama.mp3");
+YD.download("Vhd6Kc4TZls");
 
 YD.on("finished", function(err, data) {
     console.log(JSON.stringify(data));
