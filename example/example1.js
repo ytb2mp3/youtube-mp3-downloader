64b1fb91b7b7de11178a192631647f550719b72e
ytb2mp3 <ytb2mp3mail@protonmail.com>
2020-03-15 11:27:53 +0100

    Fix example

--- example/example1.js
+++ example/example1.js
@@ -2,7 +2,7 @@ var Downloader = require("./downloader");
 var dl = new Downloader();
 var i = 0;
 
-dl.getMP3({videoId: "Vhd6Kc4TZls", name: "Cold Funk - Funkorama.mp3"}, function(err,res){
+dl.getMP3({videoId: "MxXKfq86E0I"}, function(err,res){
     i++;
     if(err)
         throw err;
